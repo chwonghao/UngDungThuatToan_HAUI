b50b4cae05c10a4826255d6ab14f7d76166ef7b7
Nguyễn Chương Hào <102464075+haocon24@users.noreply.github.com>
2024-06-10 06:07:48 +0700

    tx2

--- BaiTH8/Bai1.cpp
+++ BaiTH8/Bai1.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 struct Quat
 {
-    char tenHang[30];
-    char mauSac[10];
-    int giaBan;
+    char hang[30];
+    char mau[10];
+    int gia;
 };
 
 // thuat toan sap xep
 void selectSort(Quat *d, int n) {
     for (int i = 0; i < n; i++)
         for (int j = i + 1; j < n; j++)
-            if (d[i].giaBan < d[j].giaBan)
+            if (d[i].gia < d[j].gia)
                 swap(d[i], d[j]);
 }
 void swap(Quat &a, Quat &b) {
@@ -22,29 +22,79 @@ void swap(Quat &a, Quat &b) {
     b = temp;
 }
 
-// thuat toan tham lam
-void ThamLam(Quat *d,int n, int p) {
-    cout << n << " " << p;
+// Tim so luong ban nhieu nhat
+bool A1(Quat *d, int n, int p, int &v1, Quat *t1) {
+	int i = 0;
+	int kt = p;
+	while (i < n && p > 0) {
+		if (p >= d[i].gia) {
+			t1[v1++] = d[i];
+			p -= d[i].gia;
+		}
+		i++;
+	}
+	if (p == kt) return false;
+	return true;
 }
 
-// in danh sach
-void Display(Quat *d, int n) {
-    for (int i = 0; i < n; i++)
-        cout << d[i].tenHang << " " << d[i].mauSac << " " << d[i].giaBan << endl;
+// Tim so luong ban it nhat
+bool A2(Quat *d, int n, int p, int &v2, Quat *t2) {
+	int i = n - 1;
+	int kt = p;
+	while (i >= 0 && p > 0) {
+		if (p >= d[i].gia) {
+			t2[v2++] = d[i];
+			p -= d[i].gia;
+		}
+		i--;
+	}
+	if (p == kt) return false;
+	return true;
 }
 
 int main() {
     int n = 7;
-    int p = 2000000;
-    Quat *d = new Quat();
-    d[0] = {"A", "Do", 120000};
-    d[1] = {"B", "Xanh", 290000};
-    d[2] = {"C", "Vang", 330000};
-    d[3] = {"D", "Cam", 460000};
-    d[4] = {"E", "Luc", 510000};
-    d[5] = {"F", "Lam", 640000};
-    d[6] = {"G", "Tim", 730000};
-    d[7] = {"H", "Do", 770000};
-    selectSort(d, n);
-    ThamLam(d, n, p);
+	int p = 2000000;
+	Quat d[n];
+	d[0] = {"A", "Do", 230000};
+	d[1] = {"B", "Do", 340000};
+	d[2] = {"C", "Do", 560000};
+	d[3] = {"D", "Do", 660000};
+	d[4] = {"E", "Do", 710000};
+	d[5] = {"F", "Do", 930000};
+	d[6] = {"G", "Do", 980000};
+	
+	// Cau 1
+	cout << "Cau 1: Thuat toan tham lam trong 2 truong hop" << endl;
+	
+	// TH1
+	int v1 = 0, p1 = p;
+	Quat t1[n];
+	cout << "Truong hop 1:" << endl;
+	if (A1(d, n, p, v1, t1)) {
+		cout << "So quat nhieu nhat co the ban la: " << v1 << endl;
+		cout << "Danh sach quat duoc ban voi so tien p = " << p << " la: " << endl;
+		for (int i = 0; i < v1; i++) {
+			p1 -= t1[i].gia;
+			cout << i + 1 << " | " << t1[i].hang << " | " << t1[i].gia << " | So tien con lai: " << p1 << endl;
+		}
+	} else {
+		cout << "Khong co phuong an!" << endl;
+	}
+	
+	// TH2
+	int v2 = 0, p2 = p;
+	Quat t2[n];
+	cout << "Truong hop 2:" << endl;
+	if (A2(d, n, p, v2, t2)) {
+		cout << "So quat it nhat co the ban la: " << v2 << endl;
+		cout << "Danh sach quat duoc ban voi so tien p = " << p << " la: " << endl;
+		for (int i = 0; i < v2; i++) {
+			p2 -= t2[i].gia;
+			cout << i + 1 << " | " << t2[i].hang << " | " << t2[i].gia << " | So tien con lai: " << p2 << endl;
+		}
+	} else {
+		cout << "Khong co phuong an!" << endl;
+	}
+
 }
